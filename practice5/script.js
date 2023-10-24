92486a2d9021a74afd619ac9f248f90d1fe5d81b
dionysiusmukha <115173369+dionysiusmukha@users.noreply.github.com>
2023-10-24 23:14:35 +0300

    123

---
+++ practice5/script.js
@@ -0,0 +1,20 @@
+const calc = document.getElementById('calc');
+calc.addEventListener('submit', doClick);
+
+function doClick(event) {
+  event.preventDefault();
+  const Count = calc.querySelector('[name="first"]');
+  const Good = calc.querySelector('[name="select1"]');
+  const Result = calc.querySelector('[name="result"]');
+  var price;
+  if (Good.value == 'S.T.A.L.K.E.R.: Shadow of Chernobyl') {
+    price = 25;
+  } else if (Good.value == 'S.T.A.L.K.E.R.: Clear Sky') {
+    price = 50;
+  } else if (Good.value == 'S.T.A.L.K.E.R.: Call of Pripyat') {
+    price = 75;
+  } else alert('Товар не выбран');
+  if (Count.value > 0) {
+    Result.value = Count.value * price;
+  } else alert('Количество товаров должно быть положительным');
+}
