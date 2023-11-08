eb8f6521424dc92bff50b8690735ca0274ef4226
dionysiusmukha <115173369+dionysiusmukha@users.noreply.github.com>
2023-11-08 11:46:54 +0300

    Update script.js

--- practice6/script.js
+++ practice6/script.js
@@ -3,11 +3,7 @@ const radio = document.getElementById('radio');
 const radio1 = document.getElementById('radio1');
 const radio2 = document.getElementById('radio2');
 const radio3 = document.getElementById('radio3');
-window.addEventListener('DOMContentLoaded', function (event) {
-FormChangeHandler();
-}
-form.addEventListener('change', getFormValue);
-radio.addEventListener('change', FormChangeHandler);
+
 
 function FormChangeHandler() {
   if (radio1.checked) {
@@ -40,4 +36,9 @@ function getFormValue(event) {
     if (CheckBox.checked) Result.value = Number.value * 3500 + 500;
     else Result.value = Number.value * 3500;
   }
+}
+window.addEventListener('DOMContentLoaded', function (event) {
+FormChangeHandler();
+form.addEventListener('change', getFormValue);
+radio.addEventListener('change', FormChangeHandler);
 }
