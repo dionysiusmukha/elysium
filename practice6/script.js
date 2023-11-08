671a3a531055fdd6e6ba9e70c9e92f20f3db369c
dionysiusmukha <115173369+dionysiusmukha@users.noreply.github.com>
2023-11-08 12:06:05 +0300

    Update script.js

--- practice6/script.js
+++ practice6/script.js
@@ -1,15 +1,3 @@
-const form = document.getElementById('form');
-const radio = document.getElementById('radio');
-const radio1 = document.getElementById('radio1');
-const radio2 = document.getElementById('radio2');
-const radio3 = document.getElementById('radio3');
-
-window.addEventListener('DOMContentLoaded', function () {
-  FormChangeHandler();
-  form.addEventListener('change', getFormValue);
-  radio.addEventListener('change', FormChangeHandler);
-}
-
 function FormChangeHandler() {
   if (radio1.checked) {
     document.getElementById('Select').hidden = true;
@@ -42,4 +30,16 @@ function getFormValue(event) {
     else Result.value = Number.value * 3500;
   }
 }
+
+
+window.addEventListener('DOMContentLoaded', function () {
+  FormChangeHandler();
+  form.addEventListener('change', getFormValue);
+  radio.addEventListener('change', FormChangeHandler);
+  const form = document.getElementById('form');
+  const radio = document.getElementById('radio');
+  const radio1 = document.getElementById('radio1');
+  const radio2 = document.getElementById('radio2');
+  const radio3 = document.getElementById('radio3');
+});
 
