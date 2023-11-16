f01e87b5d467a7120391df769a11ae6a1604e98d
dionysiusmukha <115173369+dionysiusmukha@users.noreply.github.com>
2023-11-16 08:47:33 +0300

    Update script.js

--- practice6/script.js
+++ practice6/script.js
@@ -17,18 +17,19 @@ function getFormValue(event) {
   const Edition = form.querySelector('[name ="edition"]');
   const Result = form.querySelector('[name ="result"]');
   const CheckBox = form.querySelector('[name ="checkbox"]');
-
-  if (radio1.checked) {
-    Result.value = Number.value * 2000;
-  } else if (radio2.checked) {
-    if (Edition.value === 'standard') Result.value = 2000 * Number.value * 1.5;
-    else if (Edition.value === 'gold') Result.value = 2000 * Number.value * 1.7;
-    else if (Edition.value === 'deluxe') Result.value = 2000 * Number.value * 2;
-    else Result.value = 'Выберите тип';
-  } else if (radio3.checked) {
-    if (CheckBox.checked) Result.value = Number.value * 3500 + 500;
-    else Result.value = Number.value * 3500;
-  }
+  if (Number.value > 0) {
+    if (radio1.checked) {
+      Result.value = Number.value * 2000;
+    } else if (radio2.checked) {
+      if (Edition.value === 'standard') Result.value = 2000 * Number.value * 1.5;
+      else if (Edition.value === 'gold') Result.value = 2000 * Number.value * 1.7;
+      else if (Edition.value === 'deluxe') Result.value = 2000 * Number.value * 2;
+      else Result.value = 'Выберите тип';
+    } else if (radio3.checked) {
+      if (CheckBox.checked) Result.value = Number.value * 3500 + 500;
+      else Result.value = Number.value * 3500;
+    }
+  else alert('Количество товаров должно быть положительным');
 }
 
 
